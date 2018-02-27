c7440edf8e604d4f187c2b9b4b9ffc8cdb00a158
Han Xiao <han.xiao@aalto.fi>
2018-02-27 07:10:20 +0000

    maximal matching

--- graph_helpers.py
+++ graph_helpers.py
@@ -1,8 +1,16 @@
+from graph_tool import Graph
+
 
 def gt_int_nodes(g):
     """return list of nodes as integer in g
     """
     return list(sorted(map(int, g.vertices())))
+
+
+def gt_int_edges(g):
+    """return list of edges as (int, int) in g
+    """
+    return list(sorted(map(lambda e: tuple(sorted(e)), g.edges())))
 
 
 def edge_induced_subgraph(g, edges):
@@ -14,10 +22,37 @@ def edge_induced_subgraph(g, edges):
 
     note that the induced subgraph might be disconnected
     """
+    vfilt = g.new_vertex_property('bool')
+    node_subset = {u for e in edges for u in e}
+    for v in node_subset:
+        vfilt[v] = True
+
+    new_g = Graph(directed=False)
+    new_g.add_vertex(g.num_vertices())
+    new_g.set_vertex_filter(vfilt)
+    new_g.add_edge_list(edges)
+    return new_g
     
 
 def maximal_matching(g):
     """given graph `g`, perform greedy matching algorithm, which gives factor-2 approximation"""
-    
+    edges = []
+    to_match = set(g.vertices())
+    while len(to_match) > 0:
+        v = next(iter(to_match))
+        try:
+            e = next(v.out_edges())
+            
+            s, t = e.source(), e.target()
+            edges.append(tuple(sorted((int(s), int(t)))))
+            to_match.remove(s)
+            to_match.remove(t)
+        except StopIteration:
+            to_match.remove(v)
+
+    return edges
 
 
+def graph_equal(g1, g2):
+    return (set(gt_int_nodes(g1)) == set(gt_int_nodes(g2)) and
+            set(gt_int_edges(g1)) == set(gt_int_edges(g2)))
--- test_graph_helpers.py
+++ test_graph_helpers.py
@@ -1,22 +1,38 @@
 import pytest
-from graph_tool import Graph, GraphView
+from graph_tool import Graph
 
-from graph_helpers import edge_induced_subgraph, gt_int_nodes
+from graph_helpers import (edge_induced_subgraph,
+                           gt_int_nodes, gt_int_edges,
+                           graph_equal, maximal_matching)
+
 
 @pytest.fixture
 def g():
-    """    
     """
-    G = Graph()
-    G.add_vertices(4)
+    """
+    G = Graph(directed=False)
+    G.add_vertex(4)
     return G
 
 
-def test_edge_induced_subgraph(g):
-    edges1 = [(0, 1)]
-    g1 = edge_induced_subgraph(g, edges1)
-    assert gt_int_nodes(g1) == [0, 1]
+@pytest.mark.parametrize("edges, nodes", [([(0, 1)], [0, 1]),
+                                          ([(0, 1), (2, 3)], [0, 1, 2, 3])])
+def test_edge_induced_subgraph(g, edges, nodes):
+    old_g = Graph(g)  # copy it
+    
+    new_g = edge_induced_subgraph(g, edges)
+    assert gt_int_nodes(new_g) == nodes
+    assert gt_int_edges(new_g) == edges
+    assert not new_g.is_directed()
 
-    edges2 = [(0, 1), (2, 3)]
-    g2 = edge_induced_subgraph(g, edges2)
-    assert gt_int_nodes(g2) == [0, 1, 2, 3]
+    assert graph_equal(old_g, g)  # g is not changed
+
+
+@pytest.mark.parametrize("graph_edges, expected_edges",
+                         [([(0, 1)], [(0, 1)]),
+                          ([(0, 1), (2, 3)], [(0, 1), (2, 3)]),
+                          ([(0, 1), (1, 2), (2, 3)], [(0, 1), (2, 3)])])
+def test_maximal_matching_simple(g, graph_edges, expected_edges):
+    g.add_edge_list(graph_edges)
+    actual_edges = maximal_matching(g)
+    assert actual_edges == expected_edges
