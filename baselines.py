fe45dee2b0c5ad931769682dc91a851c8feefdd6
Lorenzo <loseverini@gmail.com>
2018-04-17 12:15:31 +0200

    Add LR baselines

--- baselines.py
+++ baselines.py
@@ -1,7 +1,8 @@
 import numpy as np
 import implicit
 from graph_tool.spectral import adjacency
-
+from operator import itemgetter
+import networkx as nx
 
 class MFRecommender():
     def __init__(self, g):
@@ -31,3 +32,32 @@ class RandomRecommender():
         sel_ids = np.random.choice(ids, k, replace=False)
         return [cand_edges[i] for i in sel_ids]
     
+class CommonNeighbors():
+    def __init__(self, g):
+        self.g = g
+    def get_top_k_edges(self, cand_edges, k=100):
+        cand_edges = list(cand_edges)
+        preds = nx.resource_allocation_index(self.g, cand_edges)
+        #preds.sort(key=itemgetter(3),reverse=True)
+        top=sorted(preds,key=itemgetter(2),reverse=True)
+        return [(u,v) for u, v, p in top[:k]]
+    
+class AdamicAdar():
+    def __init__(self, g):
+        self.g = g
+    def get_top_k_edges(self, cand_edges, k=100):
+        cand_edges = list(cand_edges)
+        preds = nx.adamic_adar_index(self.g, cand_edges)
+
+        top=sorted(preds,key=itemgetter(2),reverse=True)
+      #  preds.sort(key=itemgetter(3),reverse=True)
+        return [(u,v) for u, v, p in top[:k]]
+class Jaccard():
+    def __init__(self, g):
+        self.g = g
+    def get_top_k_edges(self, cand_edges, k=100):
+        cand_edges = list(cand_edges)
+        preds = nx.jaccard_coefficient(self.g, cand_edges)
+        
+        top=sorted(preds,key=itemgetter(2),reverse=True)
+        return [(u,v) for u, v, p in top[:k]]
--- test_baselines.py
+++ test_baselines.py
@@ -2,6 +2,7 @@ import os
 import pickle as pkl
 import pytest
 from graph_tool import load_graph
+import networkx as nx
 
 from baselines import MFRecommender, RandomRecommender
 
@@ -9,7 +10,9 @@ os.environ['OPENBLAS_NUM_THREADS'] = '1'
 
 
 graph_name = 'grqc'
-    
+
+def G():
+    return nx.read_edgelist('data/{}/graph.csv'.format(graph_name))
 
 @pytest.fixture
 def g():
@@ -34,3 +37,18 @@ def test_random_recommender(cand_edges):
     r = RandomRecommender()
     edges = r.get_top_k_edges(cand_edges, 10000)
     assert len(set(edges)) == 10000
+
+def test_common_neighbors(G,cand_edges):
+    r = CommonNeighbors(G)
+    edges = r.get_top_k_edges(cand_edges, 10000)
+    assert len(set(edges)) == 10000
+    
+def test_adamic_adar(G,cand_edges):
+    r = AdamicAdar(G)
+    edges = r.get_top_k_edges(cand_edges, 10000)
+    assert len(set(edges)) == 10000
+
+def test_jaccard_coefficient(G,cand_edges):
+    r = Jaccard(G)
+    edges = r.get_top_k_edges(cand_edges, 10000)
+    assert len(set(edges)) == 10000    
